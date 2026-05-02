26f8d72e482bf3fcabfbc8a25ae4d9826f227bff
Jamshidbekh <jamshidmadaminjonov5@gmail.com>
2026-05-02 13:52:24 +0500

    5dars masala

---
+++ script.js
@@ -0,0 +1,115 @@
+// let I ={
+// 	ism:"Jamshdibek",
+// familiya:"Madaminjonov",
+// yashash_joy:"namangan viloyati pop tumani Navbahor shaharchasi ",
+// oqish_joyi:"1-sonli politehnikum",
+// hozir_oqiyotgan_joy: "Najot ta'lim",
+// yoshi:18,
+// id:40775,
+// qiziqish:"sayohat",
+// model:"odam",
+// jinsi:"erkak",
+// email:"jamshidbekmadaminjonov@gmail.com",
+// contact:{
+// telegram:"https://t.me/Jambo_y08",
+// instagram:"https://instagram.com/Jambo_y08",
+// phone_number:"+998933590309"
+// }
+// }
+// console.log(I.contact.telegram);
+
+// 1. user nomli object yarating. Ichida: name va age bo‘lsin. Keyin faqat name ni console.log qiling.
+// let user ={
+// 	name:"Jamshidbek",
+// 	age:18,
+// }
+// console.log(user.name);
+
+// ====================================================================================================================================
+
+// 2. product object yarating. Ichida: title va price bo‘lsin. Keyin price ni chiqaring.
+// let product ={
+// 	title:"computer"    ,
+// 	price:"cfghjk",
+// }
+// console.log(product.price);
+
+// ====================================================================================================================================
+
+// 3. student object yarating. Ichida: name va active (true yoki false) bo‘lsin. Keyin active ni chiqaring.
+// let student ={
+// 	name:"JAmshidbek",
+// 	active: true,
+// }
+// console.log(student.active);
+
+// ====================================================================================================================================
+
+// 4. car object yarating. Ichida: brand, color, year bo‘lsin. Keyin color ni chiqaring.
+// let car ={
+// 	brand: "BMW",
+// 	color:'black',
+// 	year:2020,
+
+// }
+// console.log(car.color);
+
+// ====================================================================================================================================
+
+// 5. phone object yarating. Ichida: brand va model bo‘lsin. Keyin model ni ['model'] orqali chiqaring.
+// let phone ={
+// 	brand:"iphone",
+// 	model:"17 pro max"
+// }
+// console.log(phone["model"]);
+
+// ====================================================================================================================================
+
+// 6. user object yarating. Ichida: name va address object bo‘lsin. address ichida: city bo‘lsin. Keyin
+// city ni chiqaring.
+// let user ={
+// 	name:"JAmshibek",
+// 	adress:{city:"pop"}
+// }
+// console.log(user.adress["city"]);
+
+// ====================================================================================================================================
+
+// 7. users nomli array yarating. Ichida 2 ta object bo‘lsin (name va age bilan). Keyin birinchi
+// objectning name ni chiqaring.
+// let users = [
+// 	{
+// 		name: 'jamshiebk',
+// 		age: 18,
+// 	},
+// 	{
+// 		name: 'Shaxriyor',
+// 		age: 22,
+// 	},
+// ]
+// console.log(users[1].age
+
+// )
+
+// ====================================================================================================================================
+
+// 8. student object yarating. Ichida: name va subjects (array) bo‘lsin. subjects ichida 2 ta fan yozing.
+// Keyin birinchi fanni chiqaring.
+// let student = {
+// 	name: 'jamshid',
+// 	subjects: ['matematika', 'ingliz tili'],
+// }
+// console.log(student.subjects[0]);
+
+// ====================================================================================================================================
+
+// 9. company object yarating. Ichida: name va employees (array) bo‘lsin. employees ichida 2 ta
+// object (name va role) bo‘lsin. Keyin ikkinchi employee role ni chiqaring.
+let company ={
+	name:'JAmshidbek',
+	employes
+}
+// ====================================================================================================================================
+
+// 10. user object yarating. Ichida: name, hobbies (array), address (object) bo‘lsin. address ichida city
+// yozing. Keyin hobbies[0] va city ni chiqaring.
